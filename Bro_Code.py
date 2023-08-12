51289bf1fdc402cd81c5e295a83d9f4cd5781a5f
Vedant Borawake <118039417+vedantborawake@users.noreply.github.com>
2023-08-12 10:43:18 +0530

    Bro_Code.py

--- Bro_Code.py
+++ Bro_Code.py
@@ -1,1 +1,153 @@
+#string
 
+# print('i love pizzaaa')
+# print("It is really good!!")
+#
+# first_name = "Bro"
+# last_name = "Code"
+# full_name = first_name +" "+ last_name
+# print("Hello "+full_name)
+
+#int
+
+# age = 18
+# age = age + 1
+# print("vedant's age is: "+str(age))
+
+
+#float
+
+# height = 29.5
+# print("my height is :"+str(height)+" CM")
+# # print(type(height))
+
+
+#boolean
+
+# vedant = True
+# print("vedant ans is :"+str(vedant))
+
+
+#multiple assignment
+
+# name = "vedant"
+# age = 19
+# attractive = True
+
+# name, age, attractive = "vedant", 19, True
+#
+# print(name)
+# print(age)
+# print(attractive)
+
+# vedant = om = omkar = geet = 19
+#
+# print(vedant)
+# print(om)
+# print(omkar)
+# print(geet)
+
+
+#string methods
+
+# name = "Vedant"
+# print(len(name))
+# print(name.find("t"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count("d"))
+# print(name.replace("d","a"))
+# print(name*10)
+
+
+#type casting
+
+# x = 1 #int
+# y = 20.3 #float
+# z = "3" #str
+#
+# y = int(y)
+# z = int(z)
+# x = float(x)
+#
+# print(x)
+# print(y)
+# print(z*3)
+
+
+#user input
+
+# name = input("enter your name? ")
+# age = int(input("enter your age? "))
+# age = age + 1
+# print("your name is: "+name)
+# print(("your age is: " + str(age)
+# height = float(input("what is your height? "))
+# print("you are"+str(height)+"cm tall")
+
+
+#math function
+
+#import math
+
+# pi = 3.14
+# print(abs(pi))
+# print(math.sqrt(240))
+
+# x = 1
+# y = 2
+# z = 3
+#
+# print(max(x,y,z))
+# print(min(x,y,z))
+
+
+#string slicing
+#indexing
+
+# name = "vedantborawake"
+#
+# first_name = name[0:6]
+# last_name = name[7:15]
+# nick_name = name[0:14:3]
+# reversed_name = name[::-1]
+#
+# print(reversed_name)
+
+
+#slice
+
+# website = "http://google.com"
+# slice = slice(7,-4)
+# print(website[slice])
+
+
+#if-else statement
+
+# marks = float(input("Enter Your Marks "))
+#
+# if marks >= 90:
+#     print("You Are in Top 90, Congrulation")
+# elif marks >= 50:
+#     print("You Are in Top 50")
+# elif marks >= 35:
+#     print("you are pass")
+# else:
+#     print("you are fail")
+
+
+#logical operator
+
+# marks = int(input("enter your marks"))
+# if marks >= 35 and marks <=100:
+#     print("you are pass")
+# elif marks >= 100:
+#     print("enter valid marks")
+# else:
+#     print("you are fail!!")
+
+
+ 
